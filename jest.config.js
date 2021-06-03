498d44d5d211d3b2a765fc624c845fd3fe9453f7
Nick Hanigan <australian.nick@gmail.com>
2021-06-03 20:22:42 +0930

    Module Jest Config
    Change per module example with Atomic-TS

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
-{
-  "jest": {
-    "moduleFileExtensions": ["js", "json", "vue"],
-    "transform": {
-      "^.+\\.js$": "babel-jest",
-      "^.+\\.vue$": "vue-jest"
-    }
+
+module.exports = {
+  preset: 'jest',
+  globals: {},
+  testEnvironment: 'jsdom',
+  transform: {
+    "^.+\\.vue$": "vue-jest",
+    "^.+\\js$": "babel-jest",
+     "^.+\\.tsx?$": "ts-jest"
+  },
+  moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+  moduleNameMapper: {
+    "\\.txt$": 'identity-obj-proxy'
   }
 }
